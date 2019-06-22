00678c8b81c64e86321c42a3a24ce56242ecc17d
cxmshine <13724591964@163.com>
2019-06-22 20:30:40 +0800

    springboot快速入门

---
+++ src/main/java/cn/itcast/web/HelloController.java
@@ -0,0 +1,15 @@
+package cn.itcast.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("hello")
+    public String hello(){
+        return "hello,spring boot~";
+    }
+}
